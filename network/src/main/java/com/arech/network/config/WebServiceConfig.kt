47c6327bc2e8257c51f7012ea8b78d60c2de975c
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-08-14 18:33:13 -0400

    feature: network factory

---
+++ network/src/main/java/com/arech/network/config/WebServiceConfig.kt
@@ -0,0 +1,12 @@
+package com.arech.network.config
+
+object WebServiceConfig {
+    object Url {
+        const val REMOTE_HOST = "https://api.mercadolibre.com/sites/MLA/"
+        const val LOCAL_HOST = "http://mock.api/"
+    }
+
+    object Timeout {
+        const val CONNECT: Long = 60
+    }
+}
