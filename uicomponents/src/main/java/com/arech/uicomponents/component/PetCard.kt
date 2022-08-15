930534e48898d4608172be44d4df76c652a30c91
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-08-14 20:42:39 -0400

    asas

--- uicomponents/src/main/java/com/arech/uicomponents/component/PetCard.kt
+++ uicomponents/src/main/java/com/arech/uicomponents/component/PetCard.kt
@@ -1,11 +1,17 @@
 package com.arech.uicomponents.component
 
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -13,35 +19,96 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.arech.uicomponents.R
 
 /**
  * Created by Pili Arancibia on 14-08-22.
  */
 
 @Composable
-fun PetCard() {
+fun PetCard(attrs: AttrsPetCard) {
     Card(
         modifier = Modifier,
-        elevation = 10.dp,
-        border = BorderStroke(1.dp, Color.Black)
+        elevation = dimensionResource(id = R.dimen.ui_elevation_10),
+        border = BorderStroke(dimensionResource(id = R.dimen.ui_size_1), Color.Black)
     ) {
-        PetCardContent()
+        PetCardContent(attrs)
     }
 }
 
 @Composable
-fun PetCardContent() {
-    Row(modifier = Modifier.padding(15.dp)) {
-        Column(Modifier.padding(end = 15.dp)) {
+fun PetCardContent(attrs: AttrsPetCard) {
+    Row(modifier = Modifier.padding(dimensionResource(id = R.dimen.ui_margin_16))) {
+        Column(
+            modifier = Modifier
+                .padding(end = dimensionResource(id = R.dimen.ui_margin_16))
+                .width(dimensionResource(id = R.dimen.ui_size_89))
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(dimensionResource(id = R.dimen.ui_size_89))
+                    .fillMaxWidth(),
+                painter = painterResource(id = R.drawable.cat_example),
+                contentScale = ContentScale.Fit,
+                contentDescription = null
+            )
             Text(
-                text = "PRIMERO"
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                text = "Misifus"
             )
         }
         Column() {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Gato",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Row() {
+                    Text(
+                        modifier = Modifier.padding(end = dimensionResource(id = R.dimen.ui_margin_6)),
+                        text = "Romano",
+                        fontStyle = FontStyle.Italic,
+                        fontWeight = FontWeight.Medium,
+                        color = colorResource(id = R.color.ui_dark_gray),
+                        fontSize = 16.sp
+                    )
+                    Image(
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        painter = painterResource(id = R.drawable.ic_male_symbol_14px),
+                        contentDescription = null
+                    )
+                }
+            }
             Text(
-                text = "SEGUNDO"
+                modifier = Modifier.padding(top = dimensionResource(id = R.dimen.ui_margin_12)),
+                text = "4 años",
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Medium,
+                color = colorResource(id = R.color.ui_dark_gray),
+                fontSize = 12.sp
+            )
+            Text(
+                modifier = Modifier.padding(top = dimensionResource(id = R.dimen.ui_margin_12)),
+                fontSize = 10.sp,
+                fontStyle = FontStyle.Italic,
+                color = colorResource(id = R.color.ui_gray),
+                text = "“Este gato duerme 16 horas al día le gusta jugar con pantuflas y comer bolsas de basura nuevas, peligroso”"
             )
         }
     }
@@ -54,10 +121,27 @@ fun PetCardPreview() {
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) {
-        Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
             PetCard()
         }
     }
 }
+
+data class AttrsPetCard(
+    val name: String,
+    val animal: String,
+    val breed: String,
+    val isMale: Boolean,
+    val age: String,
+    val description: String
+)
+
+private data class GenderResource(
+    @DrawableRes val icon: Int,
+    @ColorRes val backgroundColor: Int,
+    @ColorRes val borderColor: Int
+)
